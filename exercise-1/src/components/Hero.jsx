32a06785687728e22a1cfe201feb8479ca1bb901
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-01-02 16:57:28 +0100

    removed white spaces

--- exercise-1/src/components/Hero.jsx
+++ exercise-1/src/components/Hero.jsx
@@ -3,8 +3,7 @@ export const Hero = () => {
     function MyButton(){
         return(
             <button onClick={() => console.log('clicked')}>Click Me</button>
-        )
-        
+        )    
     }
 
     return (
